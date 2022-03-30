bb60672141a075187adbe20e0296a214b6048a1f
Alex Hall <analytics@alex-hall.co.uk>
2022-03-30 12:24:15 +0100

    tidied callback

--- custom_callbacks/remoteLog.py
+++ custom_callbacks/remoteLog.py
@@ -5,15 +5,17 @@ Created on Mon Mar 28 22:46:13 2022
 """
 from keras.callbacks import Callback
 import requests
+import logging
 
-class remoteLog(Callback, api_root, api_path):
+class remoteLog(Callback):
     
     def __init__(self,
-               root=api_root,
-               path=api_path,
+               root = 'http://192.168.1.217',
+               path= '/publish/epoch/end/',
                field='data',
                headers=None,
-               send_as_json=False):
+               send_as_json=False,
+               epochs=100):
         super(remoteLog, self).__init__()
 
         self.root = root
@@ -21,16 +23,21 @@ class remoteLog(Callback, api_root, api_path):
         self.field = field
         self.headers = headers
         self.send_as_json = send_as_json
+        self.epochs = epochs
 
     def on_epoch_end(self, epoch, logs=None):
         if requests is None:
-            raise ImportError('remoteLog requires the `requests` library.')
+            raise ImportError('RemoteMonitor requires the `requests` library.')
         logs = logs or {}
         send = {}
         send['data'] = logs
         send['epoch'] = epoch
-        #send['loss'] = logs['loss']
+        send['total_epochs'] = self.epochs
+               
+        try:
+            requests.post(self.root + self.path, json=send, headers=self.headers)
         
-        
-        requests.post(self.root + self.path, json=send, headers=self.headers)
+            #TODO. ugh another blind exception but connectionerror doesnt seem to work
+        except:
+            logging.info("no connection to monitor API")
         
